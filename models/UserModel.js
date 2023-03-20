e6a75222ac6d1d2553cb2e90ab0340132eafeba3
issa2580 <issadiop2580@gmail.com>
2023-03-20 20:34:18 +0000

    hash and compare password

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,6 @@
 /* Creating user models */
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -22,6 +23,18 @@ const UserSchema = new mongoose.Schema({
         timestamps: true
     
 })
+
+UserSchema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
+
+UserSchema.methods.isCorrectPassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
 
 const User = mongoose.model('User', UserSchema)
 module.exports = User
