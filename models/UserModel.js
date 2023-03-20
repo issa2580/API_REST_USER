4a277b1344d92b3f2de98e183bcfdb2d66d07fb5
issa2580 <issadiop2580@gmail.com>
2023-03-20 14:08:31 +0000

    add user model

---
+++ models/UserModel.js
@@ -0,0 +1,27 @@
+/* Creating user models */
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    
+        timestamps: true
+    
+})
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
