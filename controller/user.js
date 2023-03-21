d9625affc25f03ffde9155f7417b04f8739d424d
issa2580 <issadiop2580@gmail.com>
2023-03-21 16:14:12 +0000

    login endpoint

--- controller/user.js
+++ controller/user.js
@@ -2,8 +2,7 @@ const User = require('../models/user')
 
 const userCtrl = {
     register: async(req, res) => {
-        const { name, email, password } = req.body;
-        
+        const { name, email, password } = req.body;        
         const UserExit = await User.findOne({ email: email})
         if (UserExit) {
             res.status(400)
@@ -15,18 +14,42 @@ const userCtrl = {
             res.json({ msg: 'Password to under'})
             return;
         }
-
         const NewUser = new User({
-            name,
-            email,
-            password
+            name: name,
+            email: email,
+            password: password
         })
         await NewUser.save();
         res.status(200)
         res.json(NewUser)
-
         console.log(NewUser);
-
+    },
+    login: async(req, res) => {
+        try {
+            const { email, password} = req.body;
+            const UserExit = await User.findOne({ email: email})
+            if (!UserExit) {
+                res.status(400)
+                res.json({ msg: "User does not exist process exit"})
+                return;
+            }
+            if(!password) {
+                res.status(400)
+                res.json({ msg: "Password required process exit"})
+                return;
+            }
+            // if (password !== UserExit.password) {
+            //     res.status(400)
+            //     res.json({ msg: "Password incorrect process exit"})
+            //     return;
+            // }
+            res.status(200)
+            res.json(UserExit)
+            console.log(UserExit);
+        } catch (error) {
+            res.status(400)
+            res.json(error)
+        }
     }
 
 }
--- routes/user.js
+++ routes/user.js
@@ -4,5 +4,6 @@ const userCtrl = require('../controller/user');
 
 /* Routing for login */
 router.post('/register', userCtrl.register);
+router.post('/login', userCtrl.login);
 
 module.exports = router
