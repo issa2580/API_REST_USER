80eda9379b3295737a70f401b11595194a2e3c60
issa2580 <issadiop2580@gmail.com>
2023-03-21 12:19:01 +0000

    endpoint registration

---
+++ controller/UserController.js
@@ -0,0 +1,36 @@
+import User from "../models/UserModel";
+import jwt from 'jsonwebtoken'
+
+const UserController = {
+    register: async(req, res) => {
+        const { name, email, password } = req.body;
+        
+        const UserExit = await User.findOne({ email: email});
+        if (UserExit) {
+            return
+            res.status(400)
+            res.json({ msg: 'User does not exit'})
+        }
+        if(password.length < 6) {
+            return
+            res.status(401)
+            res.json({ msg: 'Password to under'})
+        }
+
+        const NewUser = User.create({
+            name: name,
+            email: email,
+            password: password
+        })
+        res.status(200)
+        res.json(NewUser)
+
+        console.log(NewUser);
+
+        const token = gen
+    }
+
+}
+
+
+module.exports =  UserController
