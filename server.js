30f1eab36c3b5a192f6e9fc4a94bee88b4ff091c
issa2580 <issadiop2580@gmail.com>
2023-03-29 14:58:45 +0000

    fix deployment

--- server.js
+++ server.js
@@ -38,6 +38,13 @@ app.get('/', (req, res) => {
     res.send('api rest user manager authentication')
 })
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) =>{
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
 
 /* Port de navigation */
 const port = 8080;
