5b15cc6f18186ba704f4553ed70edafae417eac0
issa2580 <issadiop2580@gmail.com>
2024-10-15 17:56:17 +0000

    fix deployment

--- server.js
+++ server.js
@@ -12,11 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(timeout("10s"));
-app.use((req, res, next) => {
-  if (!req.timedout) next();
-});
-
 /* Connect to database mongodb */
 const URL = process.env.MONGODB_URL;
 mongoose
